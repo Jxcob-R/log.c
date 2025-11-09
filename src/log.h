eb17024531d8a56fe5b7cde5f1f3cfa10d5741e4
Jacob Ramirez <contact@jacobramirez.dev>
2025-11-10 08:35:32 +1100

    refactor: rename STRINGIFY macro to avoid potential name conflicts

--- src/log.h
+++ src/log.h
@@ -30,15 +30,21 @@ typedef void (*log_LockFn)(bool lock, void *udata);
 
 enum { LOG_TRACE, LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR, LOG_FATAL };
 
-#define _STRINGIFY(X) #X
-#define STRINGIFY(X) _STRINGIFY(X)
+#define _LOG_STRINGIFY(X) #X
+#define LOG_STRINGIFY(X) _LOG_STRINGIFY(X)
 
-#define log_trace(...) log_log(LOG_TRACE, __FILE__, "" STRINGIFY(__LINE__), __VA_ARGS__)
-#define log_debug(...) log_log(LOG_DEBUG, __FILE__, "" STRINGIFY(__LINE__), __VA_ARGS__)
-#define log_info(...)  log_log(LOG_INFO,  __FILE__, "" STRINGIFY(__LINE__), __VA_ARGS__)
-#define log_warn(...)  log_log(LOG_WARN,  __FILE__, "" STRINGIFY(__LINE__), __VA_ARGS__)
-#define log_error(...) log_log(LOG_ERROR, __FILE__, "" STRINGIFY(__LINE__), __VA_ARGS__)
-#define log_fatal(...) log_log(LOG_FATAL, __FILE__, "" STRINGIFY(__LINE__), __VA_ARGS__)
+#define log_trace(...) \
+  log_log(LOG_TRACE, __FILE__, "" LOG_STRINGIFY(__LINE__), __VA_ARGS__)
+#define log_debug(...) \
+  log_log(LOG_DEBUG, __FILE__, "" LOG_STRINGIFY(__LINE__), __VA_ARGS__)
+#define log_info(...)  \
+  log_log(LOG_INFO, __FILE__, "" LOG_STRINGIFY(__LINE__), __VA_ARGS__)
+#define log_warn(...)  \
+  log_log(LOG_WARN, __FILE__, "" LOG_STRINGIFY(__LINE__), __VA_ARGS__)
+#define log_error(...) \
+  log_log(LOG_ERROR, __FILE__, "" LOG_STRINGIFY(__LINE__), __VA_ARGS__)
+#define log_fatal(...) \
+  log_log(LOG_FATAL, __FILE__, "" LOG_STRINGIFY(__LINE__), __VA_ARGS__)
 
 const char* log_level_string(int level);
 void log_set_lock(log_LockFn fn, void *udata);
